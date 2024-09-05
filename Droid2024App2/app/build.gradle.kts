de9f5767ac0fc94390d6f3b2061202175feea415
satoyu <yu.sato@fenrir.co.jp>
2024-09-05 22:48:53 +0900

    build-logic追加

--- Droid2024App2/app/build.gradle.kts
+++ Droid2024App2/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.androidGradlePlugin)
+    alias(libs.plugins.kotlinGradlePlugin)
+//    alias(libs.plugins.android.application)
+//    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -51,19 +53,19 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+//    implementation(libs.androidx.core.ktx)
+//    implementation(libs.androidx.lifecycle.runtime.ktx)
+//    implementation(libs.androidx.activity.compose)
+//    implementation(platform(libs.androidx.compose.bom))
+//    implementation(libs.androidx.ui)
+//    implementation(libs.androidx.ui.graphics)
+//    implementation(libs.androidx.ui.tooling.preview)
+//    implementation(libs.androidx.material3)
+//    testImplementation(libs.junit)
+//    androidTestImplementation(libs.androidx.junit)
+//    androidTestImplementation(libs.androidx.espresso.core)
+//    androidTestImplementation(platform(libs.androidx.compose.bom))
+//    androidTestImplementation(libs.androidx.ui.test.junit4)
+//    debugImplementation(libs.androidx.ui.tooling)
+//    debugImplementation(libs.androidx.ui.test.manifest)
 }
---
+++ Droid2024App2/build-logic/build.gradle.kts
@@ -0,0 +1,48 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.example.droid2024app.buildlogic"
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions.jvmTarget = "17"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    implementation(libs.bundles.plugins)
+}
+
+gradlePlugin {
+    plugins {
+//        register("androidApplication") {
+//            id = "droid2024app.primitive.androidapplication"
+//            implementationClass = "com.example.droid2024app.primitive.AndroidApplicationPlugin"
+//        }
+//        register("android") {
+//            id = "droid2024app.primitive.android"
+//            implementationClass = "com.example.droid2024app.primitive.AndroidPlugin"
+//        }
+//        register("androidKotlin") {
+//            id = "droid2024app.primitive.android.kotlin"
+//            implementationClass = "com.example.droid2024app.primitive.AndroidKotlinPlugin"
+//        }
+//        register("androidCompose") {
+//            id = "droid2024app.primitive.android.compose"
+//            implementationClass = "com.example.droid2024app.primitive.AndroidComposePlugin"
+//        }
+//        register("androidHilt") {
+//            id = "droid2024app.primitive.android.hilt"
+//            implementationClass = "com.example.droid2024app.primitive.AndroidHiltPlugin"
+//        }
+//        register("androidFeature") {
+//            id = "droid2024app.convention.androidfeature"
+//            implementationClass = "com.example.droid2024app.convention.AndroidFeaturePlugin"
+//        }
+    }
+}
